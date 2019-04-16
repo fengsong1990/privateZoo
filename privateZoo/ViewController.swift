403ad49d568982cc20d5cbd19d473f8bdd7ef581
feng song <fengsong.gentleman@gmail.com>
2019-04-16 14:07:12 +0800

    Initial Commit

---
+++ privateZoo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  privateZoo
+//
+//  Created by fengsong on 2019/4/16.
+//  Copyright © 2019 fengsong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ privateZooTests/privateZooTests.swift
@@ -0,0 +1,34 @@
+//
+//  privateZooTests.swift
+//  privateZooTests
+//
+//  Created by fengsong on 2019/4/16.
+//  Copyright © 2019 fengsong. All rights reserved.
+//
+
+import XCTest
+@testable import privateZoo
+
+class privateZooTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
